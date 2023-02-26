b4dad0f667499e61e98f0eefb451b787e41b3dab
Nomboh Quentin <nombohq14@gmail.com>
2023-02-26 22:48:41 +0900

    Section 2 completed walking skelton 2 react setup

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,11 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => 
+    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
