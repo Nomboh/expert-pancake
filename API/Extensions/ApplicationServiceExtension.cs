a5c4110ead47b58434f2ca6f93fd6a232a93638e
Nomboh Quentin <nombohq14@gmail.com>
2023-02-27 10:33:30 +0900

    Section 4 Completed CQRS and MediatR implimentation for crude operation

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,29 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config){
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => 
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
+            });
+
+            services.AddMediatR(typeof(List.Handler));
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -7,16 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => 
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
-});
+builder.Services.AddApplicationService(builder.Configuration);
 
 var app = builder.Build();
 
